c16f535fb776a3eb66a8292363b1a5c9fd04e4fb
Anabelagarcia <anabelanams@gmail.com>
2023-09-14 16:51:51 -0400

    aula 14/09

--- meu-projeto-express-ejs/app.js
+++ meu-projeto-express-ejs/app.js
@@ -6,11 +6,11 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('index', { message: 'Olá, Mundo!' });
+  res.render('index', { message: 'Seja Bem-vindo À ADD!' });
 });
 
 app.get('/produtos', (req, res) => {
-  res.render('produtos', { message: 'Olá, Mundo!' });
+  res.render('produtos', { message: 'Compre aqui!' });
 });
 
 app.listen(port, () => {
