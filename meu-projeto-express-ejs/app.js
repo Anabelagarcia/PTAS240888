d974802f75abd788a63be7b8197113b27c2325b4
Anabelagarcia <anabelanams@gmail.com>
2023-11-30 16:44:58 -0400

    aula-30/11

--- meu-projeto-express-ejs/app.js
+++ meu-projeto-express-ejs/app.js
@@ -8,15 +8,15 @@ app.use(express.static('public'));
 
 const produtos = [
   {id: 1, nome: "TENIS GAZELLE BOLD", preco: "R$699,99", descricao:"Para pés mais finos, recomendamos que compre um tamanho menor do que o de costume.", imagem: "img1.jpg"},
-  {id: 2, nome: "TENIS GAZELLE BOLD", preco: "R$999,99", descricao:"Para pés mais finos, recomendamos que compre um tamanho menor do que o de costume.", imagem: "img2.jpg"},
-  {id: 3, nome: "TENIS GAZELLE BOLD", preco: "R$688,99", descricao:"Para pés mais finos, recomendamos que compre um tamanho menor do que o de costume.", imagem: "img3.jpg"},
-  {id: 4, nome: "TENIS GAZELLE BOLD", preco: "R$299,99", descricao:"Para pés mais finos, recomendamos que compre um tamanho menor do que o de costume.", imagem: "img4.jpg"},
-  {id: 5, nome: "TENIS GAZELLE BOLD", preco: "R$3999,99", descricao:"Para pés mais finos, recomendamos que compre um tamanho menor do que o de costume.", imagem: "img5.jpg"},
-  {id: 6, nome: "TENIS GAZELLE BOLD", preco: "R$599,99", descricao:"Para pés mais finos, recomendamos que compre um tamanho menor do que o de costume.", imagem: "img6.jpg"},
-  {id: 7, nome: "TENIS GAZELLE BOLD", preco: "R$899,99", descricao:"Para pés mais finos, recomendamos que compre um tamanho menor do que o de costume.", imagem: "img7.jpg"},
-  {id: 8, nome: "TENIS GAZELLE BOLD", preco: "R$999,99", descricao:"Para pés mais finos, recomendamos que compre um tamanho menor do que o de costume.", imagem: "img8.jpg"},
-  {id: 9, nome: "TENIS GAZELLE BOLD", preco: "R$199,99", descricao:"Para pés mais finos, recomendamos que compre um tamanho menor do que o de costume.", imagem: "img9.jpg"},
-  {id: 10, nome: "TENIS GAZELLE BOLD", preco: "R$6699,99", descricao:"Para pés mais finos, recomendamos que compre um tamanho menor do que o de costume.", imagem: "img10.jpg"},
+  {id: 2, nome: "TENIS NIKE ORIGINAL", preco: "R$999,99", descricao:"Para pés mais finos, recomendamos que compre um tamanho menor do que o de costume.", imagem: "img2.jpg"},
+  {id: 3, nome: "TENIS ADIDAS AZUL", preco: "R$688,99", descricao:"Para pés mais finos, recomendamos que compre um tamanho menor do que o de costume.", imagem: "img3.jpg"},
+  {id: 4, nome: "TENIS PUMA BRANCO", preco: "R$299,99", descricao:"Para pés mais finos, recomendamos que compre um tamanho menor do que o de costume.", imagem: "img4.jpg"},
+  {id: 5, nome: "TENIS ADIDAS ESPORTIVO", preco: "R$3999,99", descricao:"Para pés mais finos, recomendamos que compre um tamanho menor do que o de costume.", imagem: "img5.jpg"},
+  {id: 6, nome: "TENIS NIKE AIR", preco: "R$599,99", descricao:"Para pés mais finos, recomendamos que compre um tamanho menor do que o de costume.", imagem: "img6.jpg"},
+  {id: 7, nome: "TENIS NIKE PLUMAS", preco: "R$899,99", descricao:"Para pés mais finos, recomendamos que compre um tamanho menor do que o de costume.", imagem: "img7.jpg"},
+  {id: 8, nome: "TENIS NIKE ESPORTIVO", preco: "R$999,99", descricao:"Para pés mais finos, recomendamos que compre um tamanho menor do que o de costume.", imagem: "img8.jpg"},
+  {id: 9, nome: "TENIS PUMA BMW", preco: "R$199,99", descricao:"Para pés mais finos, recomendamos que compre um tamanho menor do que o de costume.", imagem: "img9.jpg"},
+  {id: 10, nome: "TENIS NIKE CASUAL", preco: "R$6699,99", descricao:"Para pés mais finos, recomendamos que compre um tamanho menor do que o de costume.", imagem: "img10.jpg"},
 ]
 
 function buscarProdutoPorID(id) {
@@ -30,8 +30,9 @@ app.get('/', (req, res) => {
   res.render('index', { produtos });
 });
 
-app.get('/produtos', (req, res) => {
-  res.render('produtos', { message: 'Compre aqui!' });
+app.get('/produtos/:id', (req, res) => {
+  const produto = buscarProdutoPorID(req.params.id)
+  res.render('produtos', {produto});
 });
 
 app.listen(port, () => {
